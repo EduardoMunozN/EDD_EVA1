89cce284155d3f7dc3bdcc741b94aeaa78528fb3
EduardoMunozN <124702009+EduardoMunozN@users.noreply.github.com>
2023-02-07 09:33:31 -0600

    Add files via upload

---
+++ EVA1_1_HOLAMUNDO/src/main/java/com/mycompany/eva1_1_holamundo/EVA1_1_HOLAMUNDO.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.eva1_1_holamundo;
+
+/**
+ *
+ * @author eduar
+ */
+public class EVA1_1_HOLAMUNDO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
